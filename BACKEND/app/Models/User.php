b0e05b94732f8f5473b781dcba4619f7a144ea0b
MostafaBen <bmostapha0123@gmail.com>
2024-02-06 21:16:12 +0100

    make initial file structure of project

--- BACKEND/app/Models/User.php
+++ BACKEND/app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +44,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function friends(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+    public function posts(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
