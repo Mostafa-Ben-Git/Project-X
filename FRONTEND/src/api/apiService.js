a8287cb39a04073748db1e4508ca46435adc8713
MostafaBen <bmostapha0123@gmail.com>
2024-02-18 15:45:48 +0100

    update apiService.js

--- FRONTEND/src/api/apiService.js
+++ FRONTEND/src/api/apiService.js
@@ -1,9 +1,44 @@
 import axios from "axios";
 
 const apiService = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://localhost:8000",
   withCredentials: true,
-  withXSRFToken: true,
 });
+
+apiService.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    // Check if the error is related to CSRF token
+    if (error.response && error.response.status === 419) {
+      // Try to refresh the CSRF token by making a request to /sanctum/csrf-cookie
+      try {
+        await apiService.get("/sanctum/csrf-cookie");
+        // Retry the original request
+        return apiService.request(error.config);
+      } catch (csrfError) {
+        // Handle error while refreshing CSRF token (e.g., redirect to login)
+        console.error("Error refreshing CSRF token:", csrfError);
+        throw csrfError;
+      }
+    }
+    // For other errors, just throw the original error
+    throw error;
+  }
+);
+
+// apiService.interceptors.request.use(
+//   (config) => {
+//     const accessToken = localStorage.getItem("access_token");
+
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 export default apiService;
