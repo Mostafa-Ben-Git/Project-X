a30c9ff9b0c0d040017c32cc793ad26431ee40ca
MostafaBen <bmostapha0123@gmail.com>
2024-02-18 15:45:08 +0100

    update RegisteredUserController.php

--- BACKEND/app/Http/Controllers/Auth/RegisteredUserController.php
+++ BACKEND/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
 
 class RegisteredUserController extends Controller
 {
@@ -19,13 +17,9 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): JsonResponse
+    public function store(RegisterRequest $request): JsonResponse
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
-            'password' => ['required', Rules\Password::defaults()],
-        ]);
+        $request->validated();
 
         $user = User::create([
             'name' => $request->name,
