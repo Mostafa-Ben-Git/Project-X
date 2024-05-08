d48560eb5dd497d60272bf65b5361ea2af9bcd3a
MostafaBen <bmostapha0123@gmail.com>
2024-05-08 16:36:55 +0100

    fix users api

--- BACKEND/app/Http/Controllers/Api/UserController.php
+++ BACKEND/app/Http/Controllers/Api/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return new UserResource(User::all());
+        return UserResource::collection(User::all());
     }
 
     /**
